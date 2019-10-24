173edec00ee6d08afa49b552733ff1a70073432f
Paula Tavares <paulatavaresfranca@gmail.com>
2019-10-24 11:08:11 -0300

    More functions

---
+++ part2.rkt
@@ -0,0 +1,89 @@
+;; condicional
+
+(define (lat? l)
+  (cond
+    [(null? l) #t]
+    [(non-atom? (first l)) #f]
+    [else (lat? (rest l))]
+))
+
+;; if com recursividade - função somatorio
+
+(define (soma-numeros l)
+  (if (null? l)
+    0
+    (+ (first l) (soma-numeros (rest l)))))
+
+;; com acumulador
+
+(define (soma-numeros2 l a)
+  (if (null? l)
+    a
+    (soma-numeros2 (rest l) (+ (first l) a))
+  ))
+
+;; removendo elementos da lista
+
+(define (rmembro a l)
+  (cond
+    [(null? l) '()]
+    [(eq? a (first l)) (rest l)]
+    [else (cons (first l)
+      (rmembro a (rest l)))]
+))
+
+;; substituição
+
+(define (substitui a b l)
+  (cond
+    [(null? l) l]
+    [(eq? a (first l)) (cons b (substitui a b (rest l)))]
+    [else (cons (first l) (substitui a b (rest l)))]
+))
+
+;; recursividade em estruturas complexas
+
+(define (membro*? a l)
+  (cond
+    [(null? l) #f]
+    [(list? (first l)) (or (membro*? a (first l))
+                            (membro*? a (rest l)))]
+    [(eq? a (first l)) #t]
+    [else (membro*? a (rest l))]
+))
+
+;;definindo variaveis 
+
+(let [(a 100)
+  (b 50)]
+  (gcd a b)) 
+  
+;; numero variavel de parametros
+
+(define (lista . numeros)
+  numeros)
+
+;; rest e apply 
+
+(define (soma . lista)
+  (if (null? lista)
+    0
+    (+ (first lista) (apply soma (rest lista)))
+)) 
+
+;; funções anonimas
+
+(lambda (x y) (+ x y))
+
+;; recebendo funções como parametro
+
+(remove* '(4) '(2 3 4 1 10) <=)
+
+;; função map
+
+(map (lambda (number)
+  (+ 1 number))
+  '(1 2 3 4))
+
+;; reduce
+(foldl + 0 '(1 2 3 4))
