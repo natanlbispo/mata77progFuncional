49b41116039452624c91f88dab0ecffaa667d770
Natan <natan.bispo@cubos.io>
2019-10-23 22:29:55 -0300

    Slide 1

--- A1_FP-parte-1_v3.rkt
+++ A1_FP-parte-1_v3.rkt
@@ -14,22 +14,118 @@
 
 ;; (a b) != (a.b) -> aqui é par, b não é c
 
-;; Imprime cabeça Lista
+;; Retorna cabeça Lista
 (define a '(a b c))
 (first a)
 
-;; Imprime cabeça par
+;; Retorna cabeça par
 (define par '(a . b))
 (car par)
 
-;;Imprime causa Lista
+;;Reotorna cauda Lista
 (define (cauda cau)
   (rest cau)
   )
 (cauda '(a b c))
 
-;;Imprime cauda Par
+;;Retorna cauda Par
 (define (cauda2 cau)
   (cdr cau)
   )
 (cauda2 '(1 . 2))
+
+;;Encadear cdr e car (entendi não)
+(cadr '(a b c))
+
+;;
+(define A '((a)(bc) d (e(fg))))
+(rest A)
+
+;; Juntando Listas
+(define A1 '((a)))
+(define A2 '((bc) d (e(fg))))
+(cons A1 A2)
+
+;;Juntar calda(A1) com cabeça(A2) 
+(define A3 '(a b c))
+(define A4 '(c d e))
+(append A3 A4)
+
+;;Listar
+(list 'a 'b)
+(list '(a b c))
+
+;;
+(length '(a b c))
+
+;;
+(reverse '(a b c))
+
+;;
+(list? '(a b c)) ;;#t
+
+;; Tipos Primitivos
+
+;;Booleans
+#t ;;true
+#f ;;false
+
+;;Comparação
+
+(boolean? #f)
+(= 2 (+ 1 1))
+(= 2 (+ 1 0))
+
+(eq? 1 1) ;;#t
+
+;;condicionais qualquer valor conta como certo
+(if '() "certo" "errado")
+
+;; Números
+(define n1 3.14)
+(complex? n1)
+(= n1 3.14)
+(define n2 4+9i)
+(real-part n2)
+(imag-part n2)
+;; Soma de fração
+(+ 1/3 1/5);;8/15
+;;-----
+(> 2 1) ;;#t
+(sqrt 9) ;;3
+(expt 3 2) ;;9
+;;
+(exact? 3.1);;se o número é fração
+(exact? 3+4i) ;; true
+(exact? 3.0+4i) ;; false
+;;Caracteres
+(integer->char 65)
+(char->integer #\A)
+;;Strings
+(display "Apple\n")
+
+;;
+(define (juntaString j1 j2)
+  (string-append j1 j2)
+  )
+(juntaString "um-" "dois")
+
+;;
+(define (retiraString j1 j2 j3)
+  (substring j1 j2 j3)
+  )
+(retiraString "teste" 4 5)
+
+;;
+(string-length "teste")
+
+;;
+(string? "teste") ;;#t
+(string? 1) ;;#f
+
+;;
+(pair? 1) ;;#f
+
+;;Funções Variáticas
+(* 1 2 3) ;; 1*2*3 = 6
+(< 1 2 3) ;; 1 < 2 < 3 #t
